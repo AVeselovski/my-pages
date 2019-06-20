73c55b84dc53a0e192dd9080cad509411398366f
Artur Veselovski <artur.veselovski@meiko.fi>
2019-06-20 21:11:26 +0300

    quick test

--- admin/src/api/ui.js
+++ admin/src/api/ui.js
@@ -3,6 +3,7 @@ import { ROOT_URL } from '../settings/constants';
 
 export default {
   postPage(token, data) {
+    console.log(process.env);
     return axios.post(`${ROOT_URL}/pages`, data, {
       headers: {
         Authorization: `Bearer ${token}`,
