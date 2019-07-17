6c65e2d9dc127c966c062b9994c311650dc0c77a
Artur Veselovski <artur.veselovski@meiko.fi>
2019-07-17 10:29:35 +0300

    test fix

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,11 +1,19 @@
 // core
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+// utils
+import store from './store/store';
 // components
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
